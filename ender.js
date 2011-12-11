376e4285b57d78dcf4bc6c9cea72c19c73661543
Chris Evans <cmevans2@gmail.com>
2011-12-10 19:49:59 -0800

    added in reference to mediatr's channels object for ender

--- ender.js
+++ ender.js
@@ -1,7 +1,8 @@
 var M = require('mediatr')
 ender.ender(M)
 ender.ender({
-    publish: M.publish
+    channels: M.channels
+  , publish: M.publish
   , subscribe: M.subscribe
   , installTo: M.installTo
 })
